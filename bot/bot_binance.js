2d3aa7dbc5bed74d928379f4c1f25ae5d7acc198
Tacke300 <90757126+Tacke300@users.noreply.github.com>
2025-05-28 13:28:09 +0700

    Update bot_binance.js

--- bot/bot_binance.js
+++ bot/bot_binance.js
@@ -33,7 +33,7 @@ app.get('/balance', async (req, res) => {
   }
 }); // <-- ĐÃ FIX: thêm dấu } bị thiếu
 
-app.use('/bot', express.static(path.join(__dirname, 'bot')));
+app.use('/bot', express.static(path.join(__dirname)));
 
 const cron = require('node-cron');
 addLog('>>> [Cron] Bắt đầu chạy rồi nè!');
