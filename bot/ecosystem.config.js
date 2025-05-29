84e38de4acb476cae50e259e68c69912079bc6aa
Tacke300 <90757126+Tacke300@users.noreply.github.com>
2025-05-29 12:35:22 +0700

    Create ecosystem.config.js

---
+++ bot/ecosystem.config.js
@@ -0,0 +1,13 @@
+export default {
+  apps: [
+    {
+      name: "test",
+      script: "./test.js",
+      env: {
+        PORT: 3334,
+        BINANCE_API_KEY: "cZ1Y2O0kggVEggEaPvhFcYQHS5b1EsT2OWZb8zdY9C0jGqNROvXRZHTJjnQ7OG4Q",
+        BINANCE_API_SECRET: "oU6pZFHgEvbpD9NmFXp5ZVnYFMQ7EIkBiz88aTzvmC3SpT9nEf4fcDf0pEnFzoTc",
+      },
+    },
+  ],
+};
