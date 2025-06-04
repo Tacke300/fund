951a9ac51d75fc2bf6bd5c8fe19f8ddb99a9e41c
Tacke300 <90757126+Tacke300@users.noreply.github.com>
2025-06-05 03:04:17 +0700

    Update bot01.js

--- binance_t_1/bot01.js
+++ binance_t_1/bot01.js
@@ -84,7 +84,7 @@ let nextTradeDirection = 'SHORT';
 const WEB_SERVER_PORT = 2222; // Cổng cho giao diện web
 // Đường dẫn tới file log của PM2 cho bot này (để web server đọc).
 // Đảm bảo đường dẫn này chính xác với cấu hình PM2 của bạn.
-const BOT_LOG_FILE = '/home/tacke300/.pm2/logs/bot-bina-out.log'; // Cần điều chỉnh nếu dùng PM2
+const BOT_LOG_FILE = '/home/tacke300/.pm2/logs/tung01.log'; // Cần điều chỉnh nếu dùng PM2
 // Tên của bot trong PM2, phải khớp với tên bạn đã dùng khi start bot bằng PM2.
 const THIS_BOT_PM2_NAME = 'tung01'; // Cần điều chỉnh nếu dùng PM2
 
