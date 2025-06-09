7bcf8f9304a9cfe649205d92c3316e4043f44780
Tacke300 <90757126+Tacke300@users.noreply.github.com>
2025-06-09 15:53:37 +0700

    Create config.js

---
+++ binance/config.js
@@ -0,0 +1,5 @@
+// config.js
+const API_KEY = 'cZ1Y2O0kggVEggEaPvhFcYQHS5b1EsT2OWZb8zdY9C0jGqNROvXRZHTJjnQ7OG4Q'; // Replace with your actual API Key
+const SECRET_KEY = 'oU6pZFHgEvbpD9NmFXp5ZVnYFMQ7EIkBiz88aTzvmC3SpT9nEf4fcDf0pEnFzoTc'; // Replace with your actual Secret Key
+
+export { API_KEY, SECRET_KEY };
