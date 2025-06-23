f5e596cc37d85644f61b5319a3a13960231b2107
Tacke300 <90757126+Tacke300@users.noreply.github.com>
2025-06-23 12:37:08 +0700

    Create config.js

---
+++ khoa/config.js
@@ -0,0 +1,5 @@
+// config.js
+const API_KEY = 'iWvzHgbUVpYBHprBSCMmguKj5RIv1GNjM42IuSGaQjwAdwostyz468NCd0fSZpvB'; // Replace with your actual API Key
+const SECRET_KEY = 'Ff9c1fijRFRtUACtFN5T0ZM5pzRrPFiOFkmRF0hU7vrkdVjuH7Xr9UGYwEAxSMvV'; // Replace with your actual Secret Key
+
+export { API_KEY, SECRET_KEY };
