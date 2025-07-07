8f270f82556c8fac4ee47f103a4daf0867e175e7
Tacke300 <90757126+Tacke300@users.noreply.github.com>
2025-07-08 01:03:22 +0700

    Update listtop.js

--- binance/listtop.js
+++ binance/listtop.js
@@ -13,7 +13,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const port = 1803;
+const port = 9797;
 
 app.use(express.json());
 
