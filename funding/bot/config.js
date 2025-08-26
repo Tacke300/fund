e7cc5ba935ce633cffd9e8f6e299abec2d55d9ee
Tacke300 <90757126+Tacke300@users.noreply.github.com>
2025-08-26 09:26:40 +0700

    Create config.js

---
+++ funding/bot/config.js
@@ -0,0 +1,18 @@
+// config.js
+const binanceApiKey = 'cZ1Y2O0kggVEggEaPvhFcYQHS5b1EsT2OWZb8zdY9C0jGqNROvXRZHTJjnQ7OG4Q';
+const binanceApiSecret = 'oU6pZFHgEvbpD9NmFXp5ZVnYFMQ7EIkBiz88aTzvmC3SpT9nEf4fcDf0pEnFzoTc';
+const bingxApiKey = 'p29V4jTkBelypG9Acd1t4dp6GqHwyTjYcOBq9AC501HVo0f4EN4m6Uv5F2CIr7dNaNTRvaQM0CqcPXfEFuA';
+const bingxApiSecret = 'iTkMpmySRwQSawYBU3D5uFRZhH4UBdRYLOcPVrWbdAYa0go6Nohye1n7PS4XOcOmxQXYnUs1YRei5RvLPg';
+const okxApiKey = 'c2f77f8b-a71a-41a3-8caf-3459dbdbaa0b';
+const okxApiSecret = '6337107745922F1D457C472297513220'; // ĐÃ XÁC NHẬN LẠI ĐÚNG API SECRET CỦA BẠN
+const okxPassword = 'Altf4enter$';
+const bitgetApiKey = 'bg_a1ab0142c295779ac21123d5b59378e9';
+const bitgetApiSecret = 'c12fbe21cd82274bde810b3d4aa7be778e5eee30ca5f47cf8ffc9b950787c961';
+const bitgetApiPassword = 'Altf4enter';
+
+module.exports = {
+    binanceApiKey, binanceApiSecret,
+    bingxApiKey, bingxApiSecret,
+    okxApiKey, okxApiSecret, okxPassword,
+    bitgetApiKey, bitgetApiSecret, bitgetApiPassword
+};
