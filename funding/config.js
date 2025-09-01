81b14201a6c1b7d1f65c961816eb8d096f3d537b
Tacke300 <90757126+Tacke300@users.noreply.github.com>
2025-09-01 17:29:47 +0700

    Update config.js

--- funding/config.js
+++ funding/config.js
@@ -9,10 +9,14 @@ const okxPassword = 'Altf4enter$';
 const bitgetApiKey = 'bg_a1ab0142c295779ac21123d5b59378e9';
 const bitgetApiSecret = 'c12fbe21cd82274bde810b3d4aa7be778e5eee30ca5f47cf8ffc9b950787c961';
 const bitgetApiPassword = 'Altf4enter';
+const kucoinApiKey = '68b57476df90c30001d11637';
+const kucoinApiSecret = '2582b58f-fab7-4aae-8d42-97b61023bdb6';
+const kucoinApiPassword = 'Altf4enter$';
 
 module.exports = {
     binanceApiKey, binanceApiSecret,
     bingxApiKey, bingxApiSecret,
     okxApiKey, okxApiSecret, okxPassword,
-    bitgetApiKey, bitgetApiSecret, bitgetApiPassword
+    bitgetApiKey, bitgetApiSecret, bitgetApiPassword,
+    kucoinApiKey, kucoinApiSecret, kucoinApiPassword
 };
