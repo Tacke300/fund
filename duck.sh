4c07eebedce2709585fc502a4de2f10677d0a5f1
Tacke300 <90757126+Tacke300@users.noreply.github.com>
2025-10-14 15:15:45 +0700

    Update duck.sh

--- duck.sh
+++ duck.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 echo "Updating DuckDNS..."
+
+# Lấy IP thật của VPS
 IP=$(curl -s ifconfig.me)
+
+# Gửi update tới DuckDNS
 curl -s "https://www.duckdns.org/update?domains=fundingbot&token=a1c170e9-fd2d-4112-85ad-cfd95ba0b34c&ip=$IP" -o duck.log
+
 echo "Done. Log saved to duck.log"
