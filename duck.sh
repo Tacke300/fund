c5dd8cbb25f016a8789ba24fa8de2a4f8178a9b0
Tacke300 <90757126+Tacke300@users.noreply.github.com>
2025-10-14 15:14:16 +0700

    Update duck.sh

--- duck.sh
+++ duck.sh
@@ -1,1 +1,5 @@
-echo url="https://www.duckdns.org/update?domains=fundingbot&token=a1c170e9-fd2d-4112-85ad-cfd95ba0b34c&ip=" | curl -k -o /opt/duckdns/duck.log -K -
+#!/bin/bash
+echo "Updating DuckDNS..."
+IP=$(curl -s ifconfig.me)
+curl -s "https://www.duckdns.org/update?domains=fundingbot&token=a1c170e9-fd2d-4112-85ad-cfd95ba0b34c&ip=$IP" -o duck.log
+echo "Done. Log saved to duck.log"
