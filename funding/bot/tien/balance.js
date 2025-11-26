ceb51a1758e60d9866dbac570cf88175518b8dcf
Tacke300 <90757126+Tacke300@users.noreply.github.com>
2025-11-26 21:50:32 +0700

    Update balance.js

--- funding/bot/tien/balance.js
+++ funding/bot/tien/balance.js
@@ -1,13 +1,11 @@
 // funding/bot/balance.js
-module.exports = {
-    usdtDepositAddressesByNetwork: {
-        binanceusdm: {
-            
-            'APTOS': "0xbc75b1678ae7a99412e0f231a22925c6736fd524ae7364bfe447c7c7d931c424"
-        },
-        kucoinfutures: {
-            'BEP20': "0x49c5c31c9ae14a24e4d7da84865f55353e518d14"
-        },
-        
+module.exports.usdtDepositAddressesByNetwork = {
+    // Admin Binance: Chỉ nhận mạng APTOS (User từ Kucoin gửi sang)
+    binance: {
+        'APTOS': "0xbc75b1678ae7a99412e0f231a22925c6736fd524ae7364bfe447c7c7d931c424"
+    },
+    // Admin Kucoin: Chỉ nhận mạng BEP20 (User từ Binance gửi sang)
+    kucoin: {
+        'BEP20': "0x49c5c31c9ae14a24e4d7da84865f55353e518d14"
     }
 };
