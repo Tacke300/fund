c2238e7b3d543b9804131896ed4a5c0277587508
Tacke300 <90757126+Tacke300@users.noreply.github.com>
2025-11-26 21:40:38 +0700

    Update balance.js

--- funding/bot/tien/balance.js
+++ funding/bot/tien/balance.js
@@ -2,15 +2,12 @@
 module.exports = {
     usdtDepositAddressesByNetwork: {
         binanceusdm: {
-            'BEP20': "0x47551181fcc95f8620a58a2f88b700a04f2fe13a",
+            
             'APTOS': "0xbc75b1678ae7a99412e0f231a22925c6736fd524ae7364bfe447c7c7d931c424"
         },
         kucoinfutures: {
             'BEP20': "0x49c5c31c9ae14a24e4d7da84865f55353e518d14"
         },
-        bitget: {
-            'BEP20': "0xb8d6e57971ea23c02956c6ea2e342df6cc13e2d9",
-            'APTOS': "0xe8e85b01e2f2b66de47e80ab0d7fe52b2df77aef054a5bd711c0faf3c004d880"
-        }
+        
     }
 };
