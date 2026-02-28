406dbffba50a561969822e085983f20cd12c7964
Tacke300 <90757126+Tacke300@users.noreply.github.com>
2026-02-28 12:06:51 +0700

    Update yth.js

--- yt/h/yth.js
+++ yt/h/yth.js
@@ -11,7 +11,7 @@ const port = 1111;
 
 // --- CẤU HÌNH ---
 const PLAYLIST_URL = 'https://www.youtube.com/playlist?list=PLVhVhpOTVoO069xcj_lJH2A4pgUCI-4ov';
-const MAX_THREADS = 15; // Chạy 15 luồng
+const MAX_THREADS = 10; // Chạy 15 luồng
 const COOKIE_FILE = './youtube_cookies.json';
 const BASE_TEMP_DIR = path.join(__dirname, 'temp');
 
