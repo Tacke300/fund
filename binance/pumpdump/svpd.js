263a9a75962f23c8e777e1d9a4c6dbba446e6c73
Tacke300 <90757126+Tacke300@users.noreply.github.com>
2026-02-28 07:53:43 +0700

    Update svpd.js

--- binance/pumpdump/svpd.js
+++ binance/pumpdump/svpd.js
@@ -1,6 +1,6 @@
-const TP_PERCENT = 10.0; // Chốt lời tại 1% (chưa tính đòn bẩy)
-const SL_PERCENT = 5.0; // Cắt lỗ tại 5% (chưa tính đòn bẩy)
-const MIN_VOLATILITY_TO_SAVE = 10; 
+const TP_PERCENT = 1.0; // Chốt lời tại 1% (chưa tính đòn bẩy)
+const SL_PERCENT = 3.0; // Cắt lỗ tại 5% (chưa tính đòn bẩy)
+const MIN_VOLATILITY_TO_SAVE = 3; 
 const PORT = 9000;
 const HISTORY_FILE = './history_db.json';
 const LEVERAGE_FILE = './leverage_cache.json';
