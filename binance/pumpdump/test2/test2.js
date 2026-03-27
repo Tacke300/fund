575d5bbc74b2d3297b7bcaf21ae43c0926ee9b25
Tacke300 <90757126+Tacke300@users.noreply.github.com>
2026-03-27 15:08:48 +0700

    Update test2.js

--- binance/pumpdump/test2/test2.js
+++ binance/pumpdump/test2/test2.js
@@ -2,7 +2,7 @@ const PORT = 9056;
 const HISTORY_FILE = './history_db.json';
 const LEVERAGE_FILE = './leverage_cache.json';
 const COOLDOWN_MINUTES = 15; 
-const MAX_HOLD_MINUTES = 5; // <--- SỬA SỐ PHÚT CHỐT LỆNH TẠI ĐÂY
+const MAX_HOLD_MINUTES = 30; // <--- SỬA SỐ PHÚT CHỐT LỆNH TẠI ĐÂY
 
 import WebSocket from 'ws';
 import express from 'express';
