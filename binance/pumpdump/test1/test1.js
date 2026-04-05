dac7e83e227853fedb8299d70f7d8a32139acd9b
Tacke300 <90757126+Tacke300@users.noreply.github.com>
2026-04-05 09:06:55 +0700

    Update test1.js

--- binance/pumpdump/test1/test1.js
+++ binance/pumpdump/test1/test1.js
@@ -1,8 +1,8 @@
-const PORT = 9055;
+const PORT = 7001;
 const HISTORY_FILE = './history_db.json';
 const LEVERAGE_FILE = './leverage_cache.json';
 const COOLDOWN_MINUTES = 15; 
-const MAX_HOLD_MINUTES = 1440; 
+const MAX_HOLD_MINUTES = 555555; 
 
 import WebSocket from 'ws';
 import express from 'express';
