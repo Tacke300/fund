382abfba1df8749e6dbd38f6d295db9702b5d0d8
Tacke300 <90757126+Tacke300@users.noreply.github.com>
2026-04-05 09:08:20 +0700

    Update test4.js

--- binance/pumpdump/test4/test4.js
+++ binance/pumpdump/test4/test4.js
@@ -1,8 +1,8 @@
-const PORT = 9058;
+const PORT = 7004;
 const HISTORY_FILE = './history_db.json';
 const LEVERAGE_FILE = './leverage_cache.json';
 const COOLDOWN_MINUTES = 15; 
-const MAX_HOLD_MINUTES = 1440; 
+const MAX_HOLD_MINUTES = 555555; 
 
 import WebSocket from 'ws';
 import express from 'express';
