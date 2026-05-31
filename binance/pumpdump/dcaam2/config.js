9aa36d9f0c8584ee31d6fff58ca9c83634382e08
Tacke300 <90757126+Tacke300@users.noreply.github.com>
2026-05-31 21:31:01 +0700

    Create config.js

---
+++ binance/pumpdump/dcaam2/config.js
@@ -0,0 +1,5 @@
+// config.js
+const API_KEY = 'DtAVt7AlWbd5RKf0359UcITPn0l0cw18QXrab1ZstbF2unYlK9EnfnU4nxhwMSxA'; // Replace with your actual API Key
+const SECRET_KEY = 'BOBPnbdTEjU6exB56O9cDsuAivGfFIziHKcvSoJ1eJQfMBb73NGMuYDBZdXJfb3H'; // Replace with your actual Secret Key
+
+export { API_KEY, SECRET_KEY };
