b0824d6df455868f763c7feaa0e0343d7c464ad4
Tacke300 <90757126+Tacke300@users.noreply.github.com>
2026-06-27 02:12:35 +0700

    Update config.js

--- binance/pumpdump/config.js
+++ binance/pumpdump/config.js
@@ -2,4 +2,4 @@
 const API_KEY = 'avdyl1MROd9LwslvMboE2ObXKtYX2QrUoax3TPTSzwSdqyYxkmcshXfDZOjqJ1fw'; // Replace with your actual API Key
 const SECRET_KEY = 'azfJD1UcXGBEK9LM11arXUWArap4WojhJskJkCRdQDr486WNN3697wdXUBMq1Puv'; // Replace with your actual Secret Key
 
-export { API_KEY, SECRET_KEY }; 
+export { API_KEY, SECRET_KEY };
