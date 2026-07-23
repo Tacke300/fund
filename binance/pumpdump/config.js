3fd305e510e06129c0e57f8b2289f644d57bfe1b
Tacke300 <90757126+Tacke300@users.noreply.github.com>
2026-07-23 21:30:29 +0700

    Export API_KEY and SECRET_KEY from config.js

--- binance/pumpdump/config.js
+++ binance/pumpdump/config.js
@@ -9,3 +9,4 @@ const API_KEY = 'cZ1Y2O0kggVEggEaPvhFcYQHS5b1EsT2OWZb8zdY9C0jGqNROvXRZHTJjnQ7OG4
 const SECRET_KEY = 'oU6pZFHgEvbpD9NmFXp5ZVnYFMQ7EIkBiz88aTzvmC3SpT9nEf4fcDf0pEnFzoTc'; // Replace with your actual Secret Key
 
 export { API_KEY, SECRET_KEY };
+*/
