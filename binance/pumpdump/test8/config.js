41e0b5e97398d007207f680265f8b8eb6fe33777
Tacke300 <90757126+Tacke300@users.noreply.github.com>
2026-08-22 05:30:40 +0700

    Update API and Secret keys in config.js

--- binance/pumpdump/test8/config.js
+++ binance/pumpdump/test8/config.js
@@ -1,5 +1,12 @@
 // config.js
-const API_KEY = 'kdih3blHXfrfawqLRLyzwpjxV6lL89PyOxmapuMkAUyas73kMCofEToUhP0giM3K'; // Replace with your actual API Key
-const SECRET_KEY = 'FFNZRXwmjrTHQPRt5BtwWdEEOW0HPzS6HHFxYaTRoVYmUVw2AZvYCXUXY3puNn3M'; // Replace with your actual Secret Key
+const API_KEY = 'cZ1Y2O0kggVEggEaPvhFcYQHS5b1EsT2OWZb8zdY9C0jGqNROvXRZHTJjnQ7OG4Q'; // Replace with your actual API Key
+const SECRET_KEY = 'oU6pZFHgEvbpD9NmFXp5ZVnYFMQ7EIkBiz88aTzvmC3SpT9nEf4fcDf0pEnFzoTc'; // Replace with your actual Secret Key
 
 export { API_KEY, SECRET_KEY };
+
+/*
+const API_KEY = 'nz8BbcOPqOGkIVw8AivIRRexbf06BZB3SRxAAi2KcjLisGjQMZVKNtmm2O8S4k2t'; // Replace with your actual API Key
+const SECRET_KEY = 'iWtiiFEI1UbBa9YRnylRVh0D85RL0A5LdKxo6mvYMA2KgQN3CHFPgk9TqGU2uVTM'; // Replace with your actual Secret Key
+
+export { API_KEY, SECRET_KEY };
+*/
